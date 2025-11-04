01d32ed5f298bb9e497c2b0c2cc3ddff8619917d
KonjikiNoYami <zidanalbani97gmail.com>
2025-11-04 08:55:30 +0700

    del code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
-import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
-import { HeroSection } from './src/components/HeroSection';
-import { ProfileCard } from './src/components/ProfileCard';
-import { LoginForm } from './src/components/LoginForm';
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={{ padding: 20 }}>
-        <HeroSection />
-        <ProfileCard />
-        <LoginForm />
-      </ScrollView>
-    </SafeAreaView>
-    
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
